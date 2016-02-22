28b26d94148101452f4c67284b98e704b08d2e5b
Nikhil Rane <nikhilrane1992@gamil.com>
2016-02-22 17:52:49 +0530

    create location, train, departure day, reservation models

--- IRCTC_mimics/IRCTC_mimics/models.py
+++ IRCTC_mimics/IRCTC_mimics/models.py
@@ -8,4 +8,65 @@ class UserDetail(models.Model):
 	state = models.TextField()
 	city = models.TextField()
 	pinNo = models.IntegerField()
-	mobileNo = models.IntegerField()
+	mobileNo = models.TextField()
+
+
+class Location(models.Model):
+	lat = models.FloatField()
+	lng = models.FloatField()
+	locationText = models.CharField(max_length=50)
+
+class Train(models.Model):
+	trainNo = models.IntegerField()
+	trainName = models.CharField(max_length=100)
+	trainSource = models.ForeignKey('Location',related_name='trainSource')
+	trainDestination = models.ForeignKey('Location',related_name='trainDestination')
+	arrivalTime = models.TimeField(null=True)
+	departureTime = models.TimeField(null=True)
+	def __unicode__(self):
+		return self.trainName
+
+class DepartureDay(models.Model):
+	MONDAY = 0
+	TUESDAY = 1
+	WEDNESDAY = 2
+	THURSDAY = 3
+	FRIDAY = 4
+	SATURDAY = 5
+	SUNDAY = 6
+
+	WEEKDAYCHOICES = ((MONDAY, "Monday"), (TUESDAY, "Tuesday"), (WEDNESDAY, "Wednesday"), (THURSDAY, "Thursday"), (FRIDAY, "Friday"), (SATURDAY, "Saturday"), (SUNDAY, "Sunday"))
+	depDay = models.IntegerField(choices=WEEKDAYCHOICES)
+	train = models.ForeignKey('Train')
+
+class Reservation(models.Model):
+	MALE = 0
+	FEMALE = 1
+	GENDERCHOICES = ((MALE, 'Male'), (FEMALE, 'Female'))
+
+	SLEEPER = 0
+	AC3TIRE = 1
+	AC2TIRE = 2
+	BIRTHCHOICES = ((SLEEPER,'Sleeper'), (AC3TIRE, 'AC 3 Tire'), (AC2TIRE, 'AC 2 Tire'))
+
+	userDetail = models.ForeignKey('UserDetail')
+	firstName = models.CharField(max_length=30)
+	lastName = models.CharField(max_length=50)
+	gender = models.IntegerField(choices=GENDERCHOICES)
+	age = models.IntegerField()
+	addressLine1 = models.TextField()
+	addressLine2 = models.TextField()
+	state = models.TextField()
+	city = models.TextField()
+	pinNo = models.IntegerField()
+	mobileNo = models.TextField()
+	pnrNo = models.TextField()
+	journeyDate = models.DateTimeField()
+	className = models.CharField(max_length=50)
+	birth = models.IntegerField(choices=BIRTHCHOICES)
+	seatNo = models.IntegerField(blank=True, null=True)
+	source = models.ForeignKey('Location',related_name='source')
+	destination = models.ForeignKey('Location',related_name='destination')
+	train = models.ForeignKey('Train')
+	def __unicode__(self):
+		return self.firstName
