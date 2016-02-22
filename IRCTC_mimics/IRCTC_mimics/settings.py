3601a260f44093796ff41b55d67bc0f4b51d92e7
Nikhil Rane <nikhilrane1992@gamil.com>
2016-02-22 17:21:57 +0530

    change database path

--- IRCTC_mimics/IRCTC_mimics/settings.py
+++ IRCTC_mimics/IRCTC_mimics/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'IRCTC_mimics.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../Project_Database/irctc'),
     }
 }
 
